dd8fb2e18115f7d0346bed43dc9a6badfd321cf5
Shilpik Balpande <shilpik.balpande@sap.com>
2019-08-30 11:45:57 -0500

    Made some change

--- train.py
+++ train.py
@@ -41,16 +41,13 @@ if __name__ == "__main__":
     test_y = test[["quality"]]
 
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 3 else 0.5
 
     with mlflow.start_run():
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
-
         predicted_qualities = lr.predict(test_x)
-
         (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
-
         print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
         print("  RMSE: %s" % rmse)
         print("  MAE: %s" % mae)
@@ -61,5 +58,4 @@ if __name__ == "__main__":
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
-
         mlflow.sklearn.log_model(lr, "model")
